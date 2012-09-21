d551084916f56304ae2cabbd1d87941982e46bee
Severin Heiniger <severinheiniger@gmail.com>
2012-09-21 23:51:40 +0200

    Make the symbols in the outline react to clicks.
    When the user clicks on a symbol element in the outline, the
    corresponding symbol name in the editor will be selected.

--- src/valable/outline/ValaOutlinePage.java
+++ src/valable/outline/ValaOutlinePage.java
@@ -9,10 +9,21 @@
  */
 package valable.outline;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TreeNode;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.views.contentoutline.ContentOutlinePage;
+import org.gnome.vala.SourceLocation;
+import org.gnome.vala.SourceReference;
+import org.gnome.vala.Symbol;
 
 import valable.editors.ValaEditor;
 import valable.model.ValaProject;
@@ -35,19 +46,75 @@ public class ValaOutlinePage extends ContentOutlinePage {
 		this.editor = editor;
 	}
 
+	public ValaEditor getEditor() {
+		return editor;
+	}
+
 	@Override
 	public void createControl(Composite parent) {
 		super.createControl(parent);
 
-		ValaSource currentSource = ValaProject.getProject(
-				editor.getCurrentFile()).getSource(editor.getCurrentFile());
+		IFile currentFile = getEditor().getCurrentFile();
+		ValaSource currentSource = ValaProject.getProject(currentFile)
+				.getSource(currentFile);
 		currentSource.parse();
 
 		TreeViewer viewer = getTreeViewer();
 		viewer.setContentProvider(new ValaContentProvider());
 		viewer.setLabelProvider(new DelegatingStyledCellLabelProvider(
 				new ValaLabelProvider()));
-		viewer.addSelectionChangedListener(this);
 		viewer.setInput(currentSource);
+		viewer.addSelectionChangedListener(this);
 	}
+
+	/**
+	 * Selects the {@link Symbol} name in the {@link ValaEditor} when the user
+	 * selects it in the outline.
+	 */
+	@Override
+	public void selectionChanged(SelectionChangedEvent event) {
+		super.selectionChanged(event);
+
+		ISelection selection = event.getSelection();
+		if (!selection.isEmpty()) {
+			TreeNode treeNode = (TreeNode) ((IStructuredSelection) selection)
+					.getFirstElement();
+			Object value = treeNode.getValue();
+			if (value instanceof Symbol) {
+				Symbol symbol = (Symbol) value;
+				SourceReference sourceReference = symbol
+						.getNameSourceReference();
+				SourceLocation begin = sourceReference.getBegin();
+				SourceLocation end = sourceReference.getEnd();
+				int offset = getOffset(begin);
+				int length = end.getColumn() - begin.getColumn() + 1;
+				try {
+					getEditor().selectAndReveal(offset, length);
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	/**
+	 * Returns the offset of a {@link SourceLocation} within the document.
+	 */
+	private int getOffset(SourceLocation sourceLocation) {
+		IDocumentProvider provider = getEditor().getDocumentProvider();
+		IDocument document = provider.getDocument(getEditor().getEditorInput());
+
+		int line = sourceLocation.getLine() - 1;
+		int column = sourceLocation.getColumn() - 1;
+		int offset;
+		try {
+			offset = document.getLineOffset(line);
+		} catch (BadLocationException e) {
+			e.printStackTrace();
+			return 0;
+		}
+		offset += column;
+		return offset;
+	}
+
 }
